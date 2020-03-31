aae3ec88cf0f9dc88fe385e4211363326033793b
Vivek Reddy <vivekreddy@viveks-mac.local>
2020-03-31 07:00:34 -0700

    Initial Commit

---
+++ MyFristGitXcodeProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFristGitXcodeProject
+//
+//  Created by Vivek Reddy on 31/03/20.
+//  Copyright © 2020 Vivek Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
